92e7fdfc53cc354807e3f64a35ce002e109b3f05
maaly99 <74660174+maaly99@users.noreply.github.com>
2020-11-18 15:05:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body bgcolor="red GREY">
+   <?php
+$name=" maaly";
+$class="IT 3";
+$subject="PHP";
+$addess="khartoum";
+$ID_student="458667";
+$Social_status="single";
+$Phone_number="0963083908";
+
+
+echo"my name is:"."$name"."<br>";
+echo"<br>";
+echo'class:'."$class"."<br>";
+echo"<br>";
+echo"subject:"."$subject"."<br>";
+echo"<br>";
+echo"addess:"."$addess"."<br>";
+echo"<br>";
+echo"ID_student:"."$ID_student"."<br>";
+echo"<br>";
+echo"Social_status:"."$Social_status"."<br>";
+echo"<br>";
+echo"Phone_number:"."$Phone_number"."<br>";
+
+   ?> 
+</body>
+</html>
